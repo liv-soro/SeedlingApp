d36dcd7b2ffc5968b1f47d1f990c82ffccc84544
mgr1018 <mg1018@ic.ac.uk>
2021-03-06 18:42:16 +0000

    this is a commit yes yes

--- app/src/main/java/com/example/seedlingapp/Info.kt
+++ app/src/main/java/com/example/seedlingapp/Info.kt
@@ -1,59 +1,63 @@
 package com.example.seedlingapp
 
 import android.os.Bundle
+
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.Menu
 import android.view.View
 import android.view.ViewGroup
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.PersistableBundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.*
+import com.mancj.materialsearchbar.MaterialSearchBar
+import java.util.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
  * Use the [Info.newInstance] factory method to
  * create an instance of this fragment.
  */
-class Info : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+class Info : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+        setContentView(R.layout.fragment_info)
+        val lv = findViewById(R.id.mListView) as ListView
+        val searchBar = findViewById(R.id.searchBar) as MaterialSearchBar
+        searchBar.setHint("Search...")
+        searchBar.setSpeechMode(true)
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_info, container, false)
-    }
+        var borough = arrayOf("Camden","Kensington and Chelsea","Hammersmith and Fulham")
+        val adapter = ArrayAdapter<String> (this,android.R.layout.simple_list_item_1, borough)
+        lv.setAdapter(adapter)
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Info.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            Info().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+        searchBar.addTextChangeListener(object: TextWatcher {
+            override fun beforeTextChanged(charSequence : CharSequence,i: Int, i1:Int, i2:Int){
+
             }
+            override fun onTextChanged(charSequence : CharSequence,i: Int, i1:Int, i2:Int){
+                adapter.getFilter().filter(charSequence)
+
+            }
+            override fun afterTextChanged(editable: Editable){
+
+            }
+
+        })
+
+        lv.setOnItemClickListener(object: AdapterView.OnItemClickListener{
+            override fun onItemClick(adapterView: AdapterView<*>?,
+                view: View,
+                i: Int,
+                l: Long){
+                Toast.makeText(this@Info, adapter.getItem(i)!!.toString(),Toast.LENGTH_SHORT).show()
+            }
+
+        })
     }
 }
